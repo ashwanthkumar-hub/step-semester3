674909864ae49cabdf570d3bfc01329e36b7327a
ashwanthkumar-hub <ashwanthkumarvenkateshwarlu@gmail.com>
2026-09-12 16:33:03 +0530

    Add files via upload

---
+++ Assignment/Assignment:week4.java
@@ -0,0 +1,14 @@
+class Main{
+ static int maxProfit(int[] prices){
+  int min=prices[0],profit=0;
+  for(int i=1;i<prices.length;i++){
+   min=Math.min(min,prices[i]);
+   profit=Math.max(profit,prices[i]-min);
+  }
+  return profit;
+ }
+ public static void main(String[] args){
+  int[] prices={7,1,5,3,6,4};
+  System.out.println(maxProfit(prices));
+ }
+}
